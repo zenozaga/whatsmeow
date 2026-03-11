e9bf4bd5dbec5e2923e943a60d6fd2ec00a509a4
Randy stiven Valentin <zenozaga@gmail.com>
2026-03-10 22:59:11 -0400

    upgrade whatsmeow

--- store/sqlstore/upgrades/14-namspace-external-id.sql
+++ store/sqlstore/upgrades/14-namspace-external-id.sql
@@ -1,5 +1,7 @@
 -- v14 (compatible with v8+): Add a 'namespace' column to allow clustering devices by logical namespaces.
-ALTER TABLE whatsmeow_device ADD COLUMN namespace TEXT DEFAULT '';
+ALTER TABLE whatsmeow_device
+ADD COLUMN IF NOT EXISTS namespace TEXT DEFAULT '';
 
 -- v14 (compatible with v8+): Adds an 'external_id' column to link devices with external systems using a stable external identifier.
-ALTER TABLE whatsmeow_device ADD COLUMN external_id TEXT DEFAULT '';
+ALTER TABLE whatsmeow_device
+ADD COLUMN IF NOT EXISTS external_id TEXT DEFAULT '';
